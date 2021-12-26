2cb18d86ba3754530902736dc8a33072f5d0d327
atifbhat <94857007+atifbhat@users.noreply.github.com>
2021-12-26 17:54:46 +0530

    Add files via upload

---
+++ question2/index.js
@@ -0,0 +1,13 @@
+
+
+ var a = 2; 
+ 
+ var r = 3; 
+ 
+ var N = 10; 
+ var result=0;  
+
+result= a * Math.floor(Math.pow(r, N - 1)) ; 
+
+
+console.log(`The ${N}th term of the series is : ${result}`); 
